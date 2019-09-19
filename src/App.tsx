fbacec8560e70c8ef887a6d03ec739c1b9a46290
Daniel Segatto <danielsegatto5@gmail.com>
2019-09-19 06:50:01 -0300

    Fontes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-
 import Home from './pages/Listas/Listas';
 
 import '@ionic/react/css/core.css';
@@ -16,6 +15,7 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import './theme/variables.css';
+import './theme/fonts.scss';
 
 class App extends Component {
   render() {
